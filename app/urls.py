2d970d7bf16a3affe421d7be02f0022186001eb3
mohitsh7979 <mohitsh7979@gmail.com>
2024-08-28 22:28:56 +0530

    Update

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,6 @@ urlpatterns = [
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='app/passwordreset.html', form_class = MyPasswordResetForm ),name='password_reset'),
     path('password-reset/done',auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'),name = 'password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html' , form_class = MySetPassword),name='password_reset_confirm'),
-    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html' , form_class = MySetPassword),name='password_reset_confirm'),
     path('password-reset/complete',auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'),name = 'password_reset_complete'),
     path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
     path('cart/',views.show_cart),
